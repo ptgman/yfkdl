44b8338ed56da9008b243aa1da3b50a673aba71c
PTGM <develop@ptgm.info>
2019-10-04 03:59:04 +0900

    pip install yfkをrun.shからdownload.shに移動した

--- download.sh
+++ download.sh
@@ -8,3 +8,7 @@ fi
 
 rm -rf yfk
 git clone https://github.com/ptgman/yfk.git
+
+# install new version
+source $HOME/yfkenv/venv/bin/activate
+pip install --upgrade yfk/proj/dist/yfk-*.*.*-py3-none-any.whl
